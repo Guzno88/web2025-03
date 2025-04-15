522fc2d0bfd3ce921869c47eaab0fe7d06b77f8e
Andrij web2025-03 <andrij.demchuk.06@gmail.com>
2025-04-16 02:18:08 +0300

    C:/Program Files/Git/

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .requiredOption('-i, --input <paths...>', 'Input JSON file(s)')
+  .option('-i, --input <paths...>', 'Input JSON file(s)')
   .option('-o, --output <path>', 'Output file')
   .option('-d, --display', 'Display result in console');
 
 program.parse(process.argv);
 const options = program.opts();
 
+if (!options.input || options.input.length === 0) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
+
 let finalOutput = '';
 
 for (const inputFile of options.input) {
   if (!fs.existsSync(inputFile)) {
-    console.error(`Cannot find input file: ${inputFile}`);
+    console.error("Cannot find input file");
     continue;
   }
 
